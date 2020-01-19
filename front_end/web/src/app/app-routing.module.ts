760c80c04671180b1f3525698843085e43bab157
johao70 <perlazajohao@gmail.com>
2020-01-19 18:20:55 -0500

    fix routing problems

--- front_end/web/src/app/app-routing.module.ts
+++ front_end/web/src/app/app-routing.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { InstituteComponent } from './main/admin/institute/institute.component';
-import { CategoriesComponent } from './main/admin/categories/categories.component';
-import { AdminsComponent } from './main/user_registration/admins/admins.component';
-import { StudentsComponent } from './main/user_registration/students/students.component';
-import { MainComponent } from './main/main.component';
-import { NewBookComponent } from './main/books_catalogue/new-book/new-book.component';
-import { CatalogueComponent } from './main/books_catalogue/catalogue/catalogue.component';
+
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './main/home/home.component';
+import { CategoriesComponent } from './main/admin/categories/categories.component';
+import { InstituteComponent } from './main/admin/institute/institute.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: MainComponent },
-  { path: 'institute', component: InstituteComponent },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'admins', component: AdminsComponent },
-  { path: 'students', component: StudentsComponent },
-  { path: 'new_book', component: NewBookComponent },
-  { path: 'catalogue', component: CatalogueComponent },
-
+  { path: 'home', component: HomeComponent },
+  { path: 'admin/categoria', component: CategoriesComponent },
+  { path: 'admin/instituto', component: InstituteComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front_end/web/src/app/app.module.ts
+++ front_end/web/src/app/app.module.ts
@@ -4,30 +4,23 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { InstituteComponent } from './main/admin/institute/institute.component';
-import { CategoriesComponent } from './main/admin/categories/categories.component';
-import { AdminsComponent } from './main/user_registration/admins/admins.component';
-import { StudentsComponent } from './main/user_registration/students/students.component';
-import { NewBookComponent } from './main/books_catalogue/new-book/new-book.component';
-import { CatalogueComponent } from './main/books_catalogue/catalogue/catalogue.component';
 import { LoginComponent } from './login/login.component';
+import { CategoriesComponent } from './main/admin/categories/categories.component';
+import { InstituteComponent } from './main/admin/institute/institute.component';
+import { HomeComponent } from './main/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     HeaderComponent,
-    MainComponent,
-    InstituteComponent,
+    SidebarComponent,
+    LoginComponent,
+    HomeComponent,
     CategoriesComponent,
-    AdminsComponent,
-    StudentsComponent,
-    NewBookComponent,
-    CatalogueComponent,
-    LoginComponent
+    InstituteComponent,
   ],
   imports: [
     BrowserModule,
