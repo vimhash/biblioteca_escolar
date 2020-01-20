4a56d4fccc67a791e5d2ed35b0c0388ca75c17aa
johao70 <perlazajohao@gmail.com>
2020-01-19 19:13:12 -0500

    add more routes, changes sidebar and header

--- front_end/web/src/app/app-routing.module.ts
+++ front_end/web/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './main/home/home.component';
 import { CategoriesComponent } from './main/admin/categories/categories.component';
 import { InstituteComponent } from './main/admin/institute/institute.component';
+import { NewBookComponent } from './main/books_catalogue/new-book/new-book.component';
+import { CatalogueComponent } from './main/books_catalogue/catalogue/catalogue.component';
+import { StudentsComponent } from './main/user_registration/students/students.component';
+import { AdminsComponent } from './main/user_registration/admins/admins.component';
 
 
 const routes: Routes = [
@@ -13,6 +17,10 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'admin/categoria', component: CategoriesComponent },
   { path: 'admin/instituto', component: InstituteComponent },
+  { path: 'books_catalogue/new-book', component: NewBookComponent },
+  { path: 'books_catalogue/catalogue', component: CatalogueComponent },
+  { path: 'user_registration/admins', component: AdminsComponent },
+  { path: 'user_registration/students', component: StudentsComponent },
 ];
 
 
--- front_end/web/src/app/app.module.ts
+++ front_end/web/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { InstituteComponent } from './main/admin/institute/institute.component';
 import { HomeComponent } from './main/home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NewBookComponent } from './main/books_catalogue/new-book/new-book.component';
+import { CatalogueComponent } from './main/books_catalogue/catalogue/catalogue.component';
+import { AdminsComponent } from './main/user_registration/admins/admins.component';
+import { StudentsComponent } from './main/user_registration/students/students.component';
 
 
 @NgModule({
@@ -21,6 +25,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     HomeComponent,
     CategoriesComponent,
     InstituteComponent,
+    NewBookComponent,
+    CatalogueComponent,
+    AdminsComponent,
+    StudentsComponent,
   ],
   imports: [
     BrowserModule,
