4919b98ebd2627e506c99bd0f2751ef982dbd1a4
johao70 <perlazajohao@gmail.com>
2020-01-24 20:37:33 -0500

    fix migrations in server, bd model update

--- back_end/migrations/20191216181632_create_database_tables.js
+++ back_end/migrations/20191216181632_create_database_tables.js
@@ -28,8 +28,13 @@ exports.up = function(knex, Promise) {
   .createTable( 'libro', function( table ) {
     table.increments('libro_id');
     table.integer('estado_libro_id').references('estado_libro_id').inTable('estado_libro');
-    table.string('libro_nombre').notNullable().unique();
-    table.string('libro_autor').notNullable().unique();
+    table.string('libro_autor').notNullable();
+    table.string('libro_pais').notNullable();
+    table.string('libro_año').notNullable();
+    table.string('libro_titulo').notNullable();
+    table.string('libro_edicion').notNullable();
+    table.string('libro_editorial').notNullable();
+    table.string('libro_existencias').notNullable();
   })
 
   .createTable( 'persona', function( table ) {
@@ -46,26 +51,10 @@ exports.up = function(knex, Promise) {
   
   .createTable( 'reserva', function( table ) {
     table.increments('reserva_id');
+    table.integer('estado_reserva_id').references('estado_reserva_id').inTable('estado_reserva');
     table.integer('persona_id').references('persona_id').inTable('persona');
     table.integer('libro_id').references('libro_id').inTable('libro');
   })
-
-  // .createTable( 'libro', function( table ) {
-  //   table.increments('libro_id');
-  //   table.integer('categoria_id').references('categoria_id').inTable('categoria');
-  //   table.integer('estado_libro_id').references('estado_libro_id').inTable('estado_libro');
-  //   table.integer('institucion_id').references('institucion_id').inTable('institucion');
-  //   table.string('libro_autor').notNullable();
-  //   table.string('libro_pais').notNullable();
-  //   table.string('libro_año').notNullable();
-  //   table.string('libro_valor_estimado').notNullable();
-  //   table.string('libro_titulo').notNullable();
-  //   table.string('libro_edicion').notNullable();
-  //   table.string('libro_editorial').notNullable();
-  //   table.string('libro_existencias').notNullable();
-  //   table.string('libro_prestado').notNullable();
-  // })
-
 };
 
 exports.down = function(knex, Promise) {
