ef51c1b9cb37abf97ac40f5cc3b163ced5245c4e
johao70 <perlazajohao@gmail.com>
2020-01-27 11:28:09 -0500

    config page

--- front_end/react-web/src/pages/config.js
+++ front_end/react-web/src/pages/config.js
@@ -11,10 +11,25 @@ const Config = () => {
             <div className="ml-64">
                 <hr />
                 <main className="my-8">
-                    <p className="text-center">Bienvenido a la seccion de configuración del sistema.</p>
-    
+                    <p className="text-center">Bienvenido a la sección de configuración del sistema.</p>
 
-    
+                    <div className="flex ml-2 mr-2">
+                        <button className="flex-1 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 rounded">
+                            Agregar Tipo de Persona
+                        </button>
+
+                        <button className="flex-1 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 ml-2 rounded">
+                            Agregar Estado de Persona
+                        </button>
+
+                        <button className="flex-1 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 ml-2 rounded">
+                            Agregar Estado de Reserva
+                        </button>
+
+                        <button className="flex-1 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 ml-2 rounded">
+                            Agregar Estado de Libro
+                        </button>
+                    </div>
                 </main>
             </div>
         </div>
