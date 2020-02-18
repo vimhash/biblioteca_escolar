66836443e6dcdc697386fb438580218dfafcd3c9
AlexanderJoel <jas.vargas@yavirac.edu.ec>
2020-02-17 20:11:23 -0500

    changes movil

--- front_end/react-movil/pages/reserve.js
+++ front_end/react-movil/pages/reserve.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView, Text } from 'react-native';
-import { Table, TableWrapper, Row } from 'react-native-table-component';
-import HeaderExample from '../components/header';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableHighlight, ScrollView } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+import MenuDrawer from 'react-native-side-drawer';
 import { Link } from 'react-router-native';
 
-export default class ExampleThree extends Component {
+export default class ExampleOne extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['Name book', 'Fecha de reserva', 'Estado'],
-      widthArr: [40, 60, 80]
+      tableHead: ['Name Book', 'Fecha', 'Estado'],
+      tableData: [
+        ['1', '2', '3'],
+        ['a', 'b', 'c'],
+        ['1', '2', '3'],
+        ['a', 'b', 'c']
+      ],
+      open: false
     }
   }
+
+  toggleOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  drawerContent = () => {
+    return (
+      <TouchableOpacity onPress={this.toggleOpen} style={styles.animatedBox}>
+        <Text style={styles.closeButton}> -> </Text>
+        <View>
+          <TouchableHighlight>
+            <Link to="/library" style={styles.menuButton}>
+                <Text backgroundColor='white'>Biblioteca</Text>
+            </Link>
+          </TouchableHighlight>
+        </View>
+        <View>
+          <TouchableHighlight>
+            <Link to="/reserve" style={styles.menuButton}>
+                <Text backgroundColor='white'>Reservaciones</Text>
+            </Link>
+          </TouchableHighlight>
+        </View>
+      </TouchableOpacity>
+      
+    );
+  };
 
   render() {
     const state = this.state;
@@ -26,70 +59,132 @@ export default class ExampleThree extends Component {
 
     return (
       <View style={styles.container}>
-            <HeaderExample />
-        <Text style={styles.text}>Detalle de su reserva.</Text>
-        <ScrollView horizontal={true}>
-          <View>
-            <Table borderStyle={{borderColor: '#C1C0B9', width: '100%'}}>
-              <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.textHeader}/>
-            </Table>
-            <ScrollView style={styles.dataWrapper}>
-              <Table borderStyle={{borderColor: '#C1C0B9'}}>
-                {
-                  tableData.map((rowData, index) => (
-                    <Row
-                      key={index}
-                      data={rowData}
-                      widthArr={state.widthArr}
-                      style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
-                      textStyle={styles.textTable}
-                    />
-                  ))
-                }
-              </Table>
-            </ScrollView>
+        <MenuDrawer 
+          open={this.state.open} 
+          drawerContent={this.drawerContent()}
+          drawerPercentage={45}
+          animationTime={250}
+          overlay={true}
+          opacity={0.4}
+        >
+          <View style={{flex: 1, flexDirection: 'row'}}>
+          <TouchableOpacity onPress={this.toggleOpen} style={styles.menu}>
+            <Text style={styles.openButton}>Menu</Text>
+          </TouchableOpacity>
+          <View style={styles.header} >
+          <Text style={styles.textHeader}>Sistema de biblioteca</Text>
           </View>
-        </ScrollView>
+          <TouchableHighlight style={styles.menu}>
+                <Link to="/library" style={styles.openButton}>
+                    <Text backgroundColor='white'>Volver</Text>
+                </Link>
+              </TouchableHighlight>
+          </View>
+          <View style={styles.body}>
+            <Text style={styles.text}>Reservaciones Realizadas.</Text>
+              <ScrollView horizontal={true}>
+                <View style={styles.table}>
+                  <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                    <Row data={state.tableHead} style={styles.headerTable} textStyle={styles.textTable}/>
+                  </Table>
+                  <ScrollView >
+                  <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                  <Rows data={state.tableData} style={styles.row} textStyle={styles.textTable}/>
+                  </Table>
+                  </ScrollView>
+                </View>
+              </ScrollView>
+          </View>
+        </MenuDrawer> 
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
     width: '100%', 
     height: '100%',
-    flexDirection: 'column',
-    justifyContent:'center',
-    alignItems: 'stretch',
     backgroundColor: '#4fd1c5',
   },
-  header: { 
-    height: 70, 
-    backgroundColor: 'black' 
+  animatedBox: {
+    flex: 1,
+    backgroundColor: '#2c7a7b',
+    padding: 10
   },
-  textHeader: { 
-    textAlign: 'center', 
-    fontWeight: '100',
-    color: 'white' 
+  header: {
+    flex: 2, 
+    height: 75, 
+    backgroundColor: '#2c7a7b',
   },
-  textTable: { 
-    textAlign: 'center', 
-    fontWeight: '100' 
+  body: {
+    flex: 6,
+  },
+  table: {
+    paddingLeft: 77,
   },
   text:{
     color:'white',
     paddingLeft:'10%',
-    paddingBottom: '5%',
-    paddingTop: '8%',
+    paddingBottom: '10%',
+    paddingTop: '5%',
     fontSize: 18,
   },
-  dataWrapper: { 
-    marginTop: -1 
+  textHeader:{
+    color:'white',
+    paddingLeft:'10%',
+    paddingBottom: '5%',
+    paddingTop: '15%',
+    fontSize: 18,
   },
-  row: { 
-    height: 40, 
-    backgroundColor: '#E7E6E1' 
-  }
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  },
+  menu: {
+    flex: 0.5, 
+    height: 75, 
+    backgroundColor: '#2c7a7b',
+  },
+  openButton: {
+    color: '#000',
+    marginTop: '50%',
+    borderRadius: 100,
+    backgroundColor: 'white',
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+  },  
+  closeButton: {
+    color: 'black',
+    marginTop: '15%',
+    marginBottom: '20%',
+    marginLeft: '5%',
+    marginRight: '60%',
+    borderRadius: 100,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  menuButton: {
+    color: 'black',
+    marginTop: '5%',
+    marginHorizontal: '5%',
+    borderRadius: 100,
+    backgroundColor: 'white',
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+  },
+  headerTable: { 
+    height: 50,
+    backgroundColor: '#537791',
+  },
+  textTable: { 
+    textAlign: 'center', 
+    fontWeight: '100',
+    color: 'white' 
+  },
 });
--- front_end/react-movil/pages/virtualLibrary.js
+++ front_end/react-movil/pages/virtualLibrary.js
@@ -1,19 +1,72 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, ScrollView, TouchableHighlight } from 'react-native';
-import { Icon,Button, Card } from 'react-native-elements';
-import HeaderExample from '../components/header';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableHighlight, ScrollView } from 'react-native';
+import MenuDrawer from 'react-native-side-drawer';
+import { Card } from 'react-native-elements';
 import { Link } from 'react-router-native';
 
-// const HeaderExample = require('../components/header')
-
 export default class virtualLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  toggleOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  drawerContent = () => {
+    return (
+      <TouchableOpacity onPress={this.toggleOpen} style={styles.animatedBox}>
+        <Text style={styles.closeButton}> -> </Text>
+        <View>
+          <TouchableHighlight>
+            <Link to="/library" style={styles.menuButton}>
+                <Text backgroundColor='white'>Biblioteca</Text>
+            </Link>
+          </TouchableHighlight>
+        </View>
+        <View>
+          <TouchableHighlight>
+            <Link to="/reserve" style={styles.menuButton}>
+                <Text backgroundColor='white'>Reservaciones</Text>
+            </Link>
+          </TouchableHighlight>
+        </View>
+      </TouchableOpacity>
+      
+    );
+  };
   
   render() {
-    return (    
+    return (
       <View style={styles.container}>
-            <HeaderExample />
-        <Text style={styles.text}>Bienvenido a la biblioteca virtual.</Text>
-            <ScrollView vertical={true}>
+        <MenuDrawer 
+          open={this.state.open} 
+          drawerContent={this.drawerContent()}
+          drawerPercentage={45}
+          animationTime={250}
+          overlay={true}
+          opacity={0.4}
+        >
+          <View style={{flex: 1, flexDirection: 'row'}}>
+          <TouchableOpacity onPress={this.toggleOpen} style={styles.menu}>
+            <Text style={styles.openButton}>Menu</Text>
+          </TouchableOpacity>
+          <View style={styles.header} >
+          <Text style={styles.textHeader}>Sistema de biblioteca</Text>
+          </View>
+          <TouchableHighlight style={styles.menu}>
+                <Link to="/" style={styles.openButton}>
+                    <Text backgroundColor='white'>Salir</Text>
+                </Link>
+              </TouchableHighlight>
+          </View>
+          <View style={styles.body}>
+            <Text style={styles.text}>Bienvenido (Nombre).</Text>
+            <Text style={{marginHorizontal: 5, marginTop: 5, color: '#1a202c', backgroundColor:'#fbb6ce', paddingHorizontal: 15, paddingVertical: 5,  borderColor: '#fff', borderWidth: 2,}}>Bienvenido al catálogo, selecciona el libro que deseas resrevar, para mas infromación has click en "DETALLE".</Text>
+          <ScrollView vertical={true}>
               <Card
                 title='Name Book'
                 image={require('../assets/iconos-libros.png')}>
@@ -29,8 +82,8 @@ export default class virtualLibrary extends Component {
                 <Text style={{marginBottom: 10}}>
                   Año:
                 </Text>
-                <TouchableHighlight style={styles.containerIngresar}>
-                    <Link to="/detalle" style={ styles.button }>
+                <TouchableHighlight style={styles.button}>
+                    <Link to="/detalle" >
                         <Text >Detalle</Text>
                     </Link>
                 </TouchableHighlight>
@@ -50,8 +103,8 @@ export default class virtualLibrary extends Component {
                 <Text style={{marginBottom: 10}}>
                   Año:
                 </Text>
-                  <TouchableHighlight style={styles.containerIngresar}>
-                      <Link to="/detalle" style={ styles.button }>
+                  <TouchableHighlight style={styles.button}>
+                      <Link to="/detalle" >
                           <Text >Detalle</Text>
                       </Link>
                   </TouchableHighlight>
@@ -71,53 +124,41 @@ export default class virtualLibrary extends Component {
                 <Text style={{marginBottom: 10}}>
                   Año:
                 </Text>
-                  <TouchableHighlight style={styles.containerIngresar}>
-                      <Link to="/reserve" style={ styles.button }>
+                  <TouchableHighlight style={styles.button}>
+                      <Link to="/reserve">
                           <Text >Detalle</Text>
                       </Link>
                   </TouchableHighlight>
               </Card>
             </ScrollView>
+          </View>   
+        </MenuDrawer> 
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
     width: '100%', 
     height: '100%',
-    flexDirection: 'column',
-    justifyContent:'center',
-    alignItems: 'stretch',
     backgroundColor: '#4fd1c5',
   },
-  containerIngresar:{
-    height: 60,
-    marginLeft:'25%',
-    marginRight:'25%',
-    paddingTop:'10%',
+  animatedBox: {
+    flex: 1,
+    backgroundColor: '#2c7a7b',
+    padding: 10
   },
-  containerEmail:{
-    height: 60,
-    flexDirection:'row',
-    justifyContent:'center',
-    backgroundColor:'#ffffff',
-    marginLeft:'10%',
-    marginRight:'10%',
+  header: {
+    flex: 2, 
+    height: 75, 
+    backgroundColor: '#2c7a7b',
   },
-  containerPassword:{
-    height: 60,
-    flexDirection:'row',
-    justifyContent:'center',
-    backgroundColor:'#ffffff',
-    marginLeft:'10%',
-    marginRight:'10%',
-  },
-  icon:{
-    paddingTop: '2%',
-    paddingHorizontal: '3%',
+  body: {
+    flex: 6,
   },
   text:{
     color:'white',
@@ -126,15 +167,50 @@ const styles = StyleSheet.create({
     paddingTop: '8%',
     fontSize: 18,
   },
+  textHeader:{
+    color:'white',
+    paddingLeft:'10%',
+    paddingBottom: '5%',
+    paddingTop: '15%',
+    fontSize: 18,
+  },
   button: {
-    position: 'relative',
-    bottom: '0%',
-    marginBottom: 20,
-    borderRadius: 100,
-    backgroundColor: 'pink',
-    paddingHorizontal: 20,
-    paddingVertical: 14,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  },
+  menu: {
+    flex: 0.5, 
+    height: 75, 
+    backgroundColor: '#2c7a7b',
+  },
+  openButton: {
+    color: 'black',
+    marginTop: '50%',
+    marginHorizontal: '5%',
+    borderRadius: 100,
+    backgroundColor: 'white',
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+  },  
+  closeButton: {
+    color: 'black',
+    marginTop: '15%',
+    marginBottom: '20%',
+    marginLeft: '5%',
+    marginRight: '60%',
+    borderRadius: 100,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  menuButton: {
+    color: 'black',
+    marginTop: '5%',
+    marginHorizontal: '5%',
+    borderRadius: 100,
+    backgroundColor: 'white',
+    paddingHorizontal: 5,
+    paddingVertical: 10,
   },
 })
